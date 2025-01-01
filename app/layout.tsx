c3ae54d7a63c291041feffc5eaf67e85eccb243b
vinayak-gcc <vin242002@gmail.com>
2025-01-01 22:34:08 +0530

    Revert "Sentry Integration"
    This reverts commit 7540742651a7795532a3e9c8fa654361882f040a.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <meta name="google-site-verification" content="OrlvMUAuvv9XFQNtK1ca_3iX2-pvO-83iEay0yx08BE" />   
+      <meta name="google-site-verification" content="OrlvMUAuvv9XFQNtK1ca_3iX2-pvO-83iEay0yx08BE" />
       </head>
       <body className={inter.className} >
         <ThemeProvider
