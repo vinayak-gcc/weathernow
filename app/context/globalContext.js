8219b20a041056a7653a7bfcb1c2ce1e853fd8a9
vinayak-gcc <vin242002@gmail.com>
2025-01-01 22:39:02 +0530

    Optimization

--- app/context/globalContext.js
+++ app/context/globalContext.js
@@ -1,40 +1,26 @@
-"use client"
-
-export const revalidate = 0;
-
 import axios from "axios";
-import React, { useContext, createContext, useState, useEffect} from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 
 const GlobalContext = createContext();
 const GlobalContextUpdate = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
 
-  const url = 'https://api.geoapify.com/v1/ipinfo?&apiKey=f9e1b444125a42409c1941f6b2a15d18'
-
-  const [activeCityCoords, setActiveCityCoords] = useState([0,0]);
-  const [Lat, setLat] = useState(null);
-  const [Lon, setLon] = useState(null);
+  const url = 'https://api.geoapify.com/v1/ipinfo?&apiKey=f9e1b444125a42409c1941f6b2a15d18';
+  const [activeCityCoords, setActiveCityCoords] = useState([0, 0]);
 
   useEffect(() => {
-        fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.location) {
-            setLat(data.location.latitude);
-            setLon(data.location.longitude);
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching location data:', error);
-        });
-  }, []); 
-
-    useEffect(() => {
-        if (Lat !== null && Lon !== null) {
-            setActiveCityCoords([Lat, Lon]);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.location) {
+          setActiveCityCoords([data.location.latitude, data.location.longitude]);
         }
-    }, [Lat, Lon]); 
+      })
+      .catch((error) => {
+        console.error('Error fetching location data:', error);
+      });
+  }, []); 
 
   const [forecast, setForecast] = useState({});
   const [airQuality, setAirQuality] = useState({});
@@ -44,14 +30,12 @@ export const GlobalContextProvider = ({ children }) => {
   const fetchForecast = async (lat, lon) => {
     try {
       const res = await axios.get(`api/weather?lat=${lat}&lon=${lon}`);
-
       setForecast(res.data);
     } catch (error) {
       console.log("Error fetching forecast data: ", error.message);
     }
   };
 
-  // Air Quality
   const fetchAirQuality = async (lat, lon) => {
     try {
       const res = await axios.get(`api/pollution?lat=${lat}&lon=${lon}`);
@@ -61,36 +45,31 @@ export const GlobalContextProvider = ({ children }) => {
     }
   };
 
-  // five day forecast
   const fetchFiveDayForecast = async (lat, lon) => {
     try {
       const res = await axios.get(`api/fiveday?lat=${lat}&lon=${lon}`);
-
       setFiveDayForecast(res.data);
     } catch (error) {
       console.log("Error fetching five day forecast data: ", error.message);
     }
   };
 
-
-  //fetch uv data
   const fetchUvIndex = async (lat, lon) => {
     try {
       const res = await axios.get(`/api/uv?lat=${lat}&lon=${lon}`);
-
       setUvIndex(res.data);
     } catch (error) {
-      console.error("Error fetching the forecast:", error);
+      console.error("Error fetching the UV index:", error);
     }
   };
 
-
   useEffect(() => {
-    fetchForecast(activeCityCoords[0], activeCityCoords[1]);
-    fetchAirQuality(activeCityCoords[0], activeCityCoords[1]);
-    fetchFiveDayForecast(activeCityCoords[0], activeCityCoords[1]);
-    fetchUvIndex(activeCityCoords[0], activeCityCoords[1]);
-
+    if (activeCityCoords[0] !== 0 || activeCityCoords[1] !== 0) {
+      fetchForecast(activeCityCoords[0], activeCityCoords[1]);
+      fetchAirQuality(activeCityCoords[0], activeCityCoords[1]);
+      fetchFiveDayForecast(activeCityCoords[0], activeCityCoords[1]);
+      fetchUvIndex(activeCityCoords[0], activeCityCoords[1]);
+    }
   }, [activeCityCoords]);
 
   return (
@@ -112,8 +91,7 @@ export const GlobalContextProvider = ({ children }) => {
       </GlobalContextUpdate.Provider>
     </GlobalContext.Provider>
   );
-
-  };
+};
 
 export const useGlobalContext = () => useContext(GlobalContext);
 export const useGlobalContextUpdate = () => useContext(GlobalContextUpdate);
